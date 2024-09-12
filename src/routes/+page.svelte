604327a3d074d997aad2d53abe508e25fb3e9c7c
ATI-Ritik <kumarritik262@gmail.com>
2024-09-12 19:30:50 +0530

    Add initial project files and configurations

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+import {generations } from "./generations"
+import type { PageData } from "./$types";
+export let data: PageData;
+</script>
+
+
+{#each data.monsters as monster (monster.url)}
+<p>{monster.id} : {monster.name}</p>
+{/each}
+
+{#each generations as generation}
+<h1>{generation.name}</h1>
+<p>Games: {generation.games.join(', ')}</p>
+<p>Main region: {generation.main_region}</p>
+{/each}
+
+
+
+<style>
+
+</style>
